c973e528327c81d05a3c1df1a03f730a5564fb2d
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2022-01-21 01:25:19 +0900

    클래스 제작과 객체 생성

---
+++ JAVA 객체/Bicycle.java
@@ -0,0 +1,20 @@
+public class Bicycle {
+
+    public String color;
+    public int price;
+
+    public Bicycle(){   //default 생성자
+        System.out.println("Bicycle constructor - 1");
+    }
+    public Bicycle(String c, int p){
+        System.out.println("Bicycle constructor - 2");
+        color = c;
+        price = p;
+    }
+
+    public void info(){
+        System.out.println("--info()--");
+        System.out.println("color :"+color);
+        System.out.println("price :"+price);
+    }
+}
