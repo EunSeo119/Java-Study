8e0a49bd55e20dda7f9a903ebe84d0c30e7ee969
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2022-01-21 23:17:25 +0900

    내부 클래스와 익명 클래스

---
+++ JAVA 상속 및 클래스3/AnnoymousClass.java
@@ -0,0 +1,10 @@
+public class AnnoymousClass {
+
+    public AnnoymousClass(){
+        System.out.println("AnnoymousClass constructor");
+    }
+
+    public void method(){
+        System.out.println("-- AnnoymousClass's method START --");
+    }
+}
